245285a687df6f87bf9adaadf2ff7d3a9a8d7a91
Abhinayasrikanikella <164603507+Abhinayasrikanikella@users.noreply.github.com>
2024-03-23 15:19:56 +0530

    Add files via upload

---
+++ function overinding.py
@@ -0,0 +1,12 @@
+class father:
+    def bike(self):
+       print("harley devidson")
+    def laptop(self):
+      print("laptopeith 2gb RAM")
+class krishna(father):
+    def laptop (self):
+        print("as per my programming")
+        print("laptop  with my 2gb RAM")
+obj=krishna()
+obj.bike()
+obj.laptop()
---
+++ max and min.py
@@ -0,0 +1,9 @@
+list=[42,36,28,96,4,-1,1]
+min=999
+max=-999
+{
+if l[i]<min:
+  min=l[i]
+if l[i]>max:
+  max=l[i]
+}
